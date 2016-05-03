4a6c1f1ab8a9d253ab5a0de7dfd4799455c9a9a9
behzad farokhi <behzad@redapesolutions.com>
2016-05-03 18:28:57 +1200

    new models for triples

---
+++ app/models/triple.scala
@@ -0,0 +1,16 @@
+package models
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import pdstore.PDStore
+import pdstore.GUID
+
+object triple {
+	implicit val tripleWrites: Writes[triple] = (
+		(JsPath \ "tObject").write[String] and
+		(JsPath \ "tPredicate").write[String] and 
+		(JsPath \ "tSubject").write[String]
+	)(unlift(triple.unapply))
+}
+
+case class triple(tObject: String, tPredicate: String, tSubject: String)
