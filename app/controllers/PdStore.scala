169288c15b7e2b8ec1dd1c10fdbecd366655dc20
behzad farokhi <behzad@redapesolutions.com>
2016-05-17 20:33:26 +1200

    pdStore returns a simple Json data now.

--- app/controllers/PdStore.scala
+++ app/controllers/PdStore.scala
@@ -3,6 +3,8 @@ package controllers
 import javax.inject._
 import play.api.mvc.{Controller, Action}
 import play.api.libs.json._
+import scala.collection.mutable.ArrayBuffer	
+import scala.collection.immutable.Seq
 import models.{triple, DB}
 import pdstore._
 import pdstore.GUID
@@ -14,11 +16,11 @@ class PdStore @Inject() extends Controller {
 	var hasJob = new GUID ()
 	var hasParent = new GUID ()
 	store.setName(hasJob, "hasJob");
-	store.setName(hasParent, "hasParent");
+	store.setName(hasParent, "hasParent")
 
 	store.begin
 	store.addLink("James", hasJob, "Programmer")
-	store.addLink("Max", hasJob, "Cinematographer")
+	store.addLink("Max", hasJob, "Programmer")
 	store.addLink("Beth", hasJob, "Waiter")
 	store.addLink("Sue", hasParent, "James")
 	store.addLink("Mike", hasParent, "James")
@@ -26,6 +28,7 @@ class PdStore @Inject() extends Controller {
 	store.commit
 	
 	def add(tObject: String, tPredicate: String, tSubject: String) = Action{
+		store.begin
 		store.addLink(tObject, hasJob, tSubject)
 		store.commit
 		Ok(Json.obj("result" -> triple(tObject, tPredicate, tSubject)))
@@ -37,18 +40,24 @@ class PdStore @Inject() extends Controller {
 
 	def query(qObject: String, qPredicate: String, qSubject: String)= Action {
 		var qGuid = new GUID ()
-		var result: List[pdstore.sparql.Assignment[pdstore.GUID,AnyRef,pdstore.GUID]] = List()
+		var result = ArrayBuffer[String]()
 
 		if(qPredicate == "hasJob")
 			qGuid = hasJob
 		if(qPredicate == "hasParent")
 			qGuid = hasParent
 
-		if(qObject == "_")
-			result = store.query((v"x", qGuid, qSubject)).toList
-		if(qSubject == "_")
-			result = store.query((qObject, qGuid, v"x")).toList
-
-		Ok(Json.obj(qObject -> triple(qObject, qPredicate, result.toString)))
+		if(qObject == "_"){
+			for ( i <- 0 to (store.query((v"x", qGuid, qSubject)).toList.length - 1)) {
+				result += store.query((v"x", qGuid, qSubject)).toList(i).get(v"x").toString
+			}
+		}
+		if(qSubject == "_"){
+			for ( i <- 0 to (store.query((qObject, qGuid, v"x")).toList.length - 1)){
+				result += store.query((qObject, qGuid, v"x")).toList(i).get(v"x").toString
+			}
+		}
+		//Ok(Json.obj(qObject -> triple(qObject, qPredicate, result.toString)))
+		Ok(Json.toJson(result))
 	}
 }
