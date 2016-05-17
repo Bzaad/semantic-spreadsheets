e603b42287d59d350f9b4026f6b35bce52b04db3
behzad farokhi <behzad@redapesolutions.com>
2016-05-17 20:34:16 +1200

    added new services and values to the index.

--- ui/app/scripts/controllers/table.js
+++ ui/app/scripts/controllers/table.js
@@ -1,15 +1,16 @@
-'use strict'
+'use strict';
 
 angular.module('uiApp')
-	.controller('tableCtrl', function($scope, dataFactory){
+	.controller('tableCtrl', ['$scope', 'DataFactoryService', 'QueryService', function($scope, DataFactoryService, QueryService){
 		$scope.rowHeaders = true;
 		$scope.colHeaders = true;
+
 		$scope.db = {
-			items: dataFactory.generateArrayOfArrays(200, 20)
+			items: DataFactoryService.generateArrayOfArrays(200, 20)
 		};
 		$scope.settings = {
 			contextMenu: [
 				'row_above', 'row_below', 'remove_row'
 			]
 	};
-});
+}]);
