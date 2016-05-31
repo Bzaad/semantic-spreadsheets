d9c8589deb5e3412d764e8043030430b66f7704f
behzad farokhi <behzad@redapesolutions.com>
2016-05-31 17:49:56 +1200

    new adding service

---
+++ ui/app/scripts/services/addService.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('uiApp')
+	.factory('AddService', ['$rootScope', '$http', 'ApiUrls', function($rootScope, $http, ApiUrls){
+
+		var addTriples = function(allTriples){
+			var tripleFeeder = function(){
+				if (allTriples.length > 0){
+					postTriple(allTriples.shift());
+				} else  {
+					console.log('done adding all triples');
+				}
+			}
+			var postTriple = function(triple){
+				var triple 
+				var url = ApiUrls.baseUrl + 'triples/add/' + triple.subject + '/' + triple.predicate + '/' + triple.object; 
+				$http.post(url)
+				.success(function(response){
+					tripleFeeder();
+				})
+				.error(function(error){
+					console.log(error);
+				})
+				.finally(function(){
+					console.log('done adding the triple!');
+				})
+			}
+			tripleFeeder();
+		}
+
+		return {
+			getTriples: function(triples){
+				addTriples(triples);
+			}
+		}
+	}])
