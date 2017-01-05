7b750a32ba7432555f07955bb7522b66da81c002
Bzaad <bfar713@aucklanduni.ac.nz>
2017-01-05 19:18:12 +1300

    some experiments on loading the table directly from server

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -9,6 +9,27 @@
             };
     })();
 
+    var data = [
+        ["", "Ford", "Volvo", "Toyota", "Honda"],
+        ["2016", 10, 11, 12, 13],
+        ["2017", 20, 11, 14, 13],
+        ["2018", 30, 15, 12, 13]
+    ];
+    var container = document.getElementById("example");
+    var options = {};
+    var ht = new Handsontable(container, options);
+    console.log(ht)
+    //var container = $("#example").handsontable('getInstance');
+    //console.log(container);
+    /*
+    $container.handsontable({
+        data: data,
+        rowHeaders: true,
+        colHeaders: true,
+        contextMenu: true
+    })
+    */
+
     var socket = new WebSocket("ws://"+location.host+"/socket");
     var connected = false;
     var players = [];
