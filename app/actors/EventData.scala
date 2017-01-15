2c8c5decfce667fbc99b391a0319bf75c174a6b8
Bzaad <bfar713@aucklanduni.ac.nz>
2017-01-16 12:09:07 +1300

    EventData object for recieving changes.

---
+++ app/actors/EventData.scala
@@ -0,0 +1,25 @@
+package actors
+
+import play.api.libs.json.{JsValue, Writes, Json}
+
+/**
+  * Created by behzadfarokhi on 16/01/17.
+  */
+
+case class EventData(header: String, user: String, changes: JsValue, created: java.util.Date)
+
+// an event data Json Object for each message
+
+object EventData {
+  implicit val eventDataWrites = new Writes[EventData] {
+    def writes(eventData: EventData): JsValue = {
+      Json.obj(
+        "type" -> "change",
+        "header" -> eventData.header,
+        "user" -> eventData.user,
+        "changes" -> eventData.changes,
+        "created" -> eventData.created
+      )
+    }
+  }
+}
