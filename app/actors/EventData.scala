7e1df27254dd4ae53ff724105e6ef9ce877ff028
Bzaad <bfar713@aucklanduni.ac.nz>
2017-03-21 11:51:32 +1300

    New comments on event data

--- app/actors/EventData.scala
+++ app/actors/EventData.scala
@@ -3,13 +3,12 @@ package actors
 import play.api.libs.json.{JsValue, Writes, Json}
 
 /**
-  * Created by behzadfarokhi on 16/01/17.
+  * Event data Json Object for the messages sent and received by users
+  * Full documentation for how to handle JSON objects in Play Framework:
+  * "https://www.playframework.com/documentation/2.5.x/ScalaJson"
   */
 
 case class EventData(header: String, user: String, changes: JsValue, created: java.util.Date)
-
-// an event data Json Object for each message
-
 object EventData {
   implicit val eventDataWrites = new Writes[EventData] {
     def writes(eventData: EventData): JsValue = {
