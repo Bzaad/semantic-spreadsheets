a92114e77a543c84a84d5b9d5220c18b445cafc3
Bzaad <bfar713@aucklanduni.ac.nz>
2017-07-04 15:40:08 +1200

    Added New user group and user manager actors

--- app/models/actors.scala
+++ app/models/actors.scala
@@ -2,13 +2,51 @@ package models
 
 import akka.actor._
 import models.User._
+import models.UserManager._
 
 /**
   * Created by behzadfarokhi on 29/06/17.
   */
+
+object UserGroup {
+  def props(groupId: String): Props = Props(new UserGroup(groupId))
+}
+
+class userGroup (groupId: String) extends Actor with ActorLogging {
+  var userIdToActor = Map.empty[String, ActorRef]
+
+  override def preStart(): Unit = log.info("UserGroup {} started!", groupId)
+
+  override def postStop(): Unit = log.info("UserGroup {} stopped!", groupId)
+}
+
+object UserManager {
+
+  def props: Props = Props(new UserManager)
+
+  final case class RequestTrackUser(groupId: String, userId: String)
+  case object DeviceRegistered
+}
+
+class UserManager extends Actor with ActorLogging {
+
+  var groupIdToActor = Map.empty[String, ActorRef]
+  var actorToGroupId = Map.empty[ActorRef, String]
+
+  override def preStart(): Unit = log.info("User Manager Started!")
+
+  override def postStop(): Unit = log.info("User Manager Stopped!")
+
+  override def receive: Receive = Actor.emptyBehavior
+
+}
+
 object User {
 
   def props(groupId: String, userId: String): Props = Props(new User(groupId, userId))
+
+  final case class RecordData(requestId: Long, value: Double)
+  final case class DataRecorded(requestId: Long)
 
   final case class ReadData(requestId: Long)
   final case class RespondData(requestId: Long, value: Option[Double])
@@ -16,13 +54,28 @@ object User {
 
 class User(groupId: String, userId: String) extends Actor with ActorLogging {
 
-  val lastDataReading: Option[Double] = None
+  var lastDataReading: Option[Double] = None
 
   override def preStart(): Unit = log.info("User Actor {}-{} Started!", groupId, userId)
 
   override def postStop(): Unit = log.info("User Actor {}-{} Stoped!", groupId, userId)
 
   override def receive: Receive = {
+
+    case RequestTrackUser(`groupId`, `userId`) =>
+      sender() ! DeviceRegistered
+
+    case RequestTrackUser(groupId, userId) =>
+      log.warning(
+        "Ignoring TrackUser request for {}-{}. This actor is responsible for {}-{}.",
+        groupId, userId, this.groupId, this.userId
+      )
+
+    case RecordData (id, value) =>
+      log.info("Recorded data reading {} with {}.", value, id)
+      lastDataReading = Some(value)
+      sender() ! DataRecorded(id)
+
     case ReadData(id) =>
       sender() ! RespondData(id, lastDataReading)
   }
--- test/models/ActorsSpec.scala
+++ test/models/ActorsSpec.scala
@@ -7,6 +7,7 @@ import org.specs2.specification.Scope
 import play.api.inject._
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers._
+import scala.concurrent.duration._
 
 
 /**
@@ -30,8 +31,33 @@ class ActorsSpec extends Specification {
 
           userActor.tell(User.ReadData(requestId = 42), probe.ref)
           val response = probe.expectMsgType[User.RespondData]
-          response.requestId should ===(42)
-          response.value should ===(None)
+          response.requestId should === (42)
+          response.value should === (None)
+        }
+      }
+
+      "reply to registration requests" in new Actors{
+        running(app) {
+          val probe = TestProbe()
+          val userActor = actorSystem.actorOf(User.props("group", "user"))
+
+          userActor.tell(UserManager.RequestTrackUser("group", "user"), probe.ref)
+          probe.expectMsg(UserManager.UserRegistered)
+          probe.lastSender should === (userActor)
+
+        }
+      }
+
+      "ignore wrong regesteration requests" in new Actors{
+        running(app) {
+          val probe = TestProbe()
+          val userActor = actorSystem.actorOf(User.probs("group", "user"))
+
+          userActor.tell(UserManager.RequestTrackUser("wrongGroup", "user"), probe.ref)
+          probe.expectNoMsg(500.milliseconds)
+
+          userActor.tell(UserManager.RequestTrackUser("group", "wrongUser"), probe.ref)
+          probe.expectNoMsg(500.millisecond)
         }
       }
 
