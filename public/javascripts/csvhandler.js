835a69b3c44c416e5bc160186c624d792f054dfb
behzad <bfar713@aucklanduni.ac.nz>
2018-03-19 16:51:39 +1300

    Properly creating the CSV structure for export (the file exportation bit is not yet implemented!)

--- public/javascripts/csvhandler.js
+++ public/javascripts/csvhandler.js
@@ -3,51 +3,66 @@ const requestExportCsv = tId =>{
 };
 
 const tableToCsv = tableTriples => {
-    console.log(tableTriples);
     let rows = [];
     let columns = [];
     let subPreVals = [];
+    let objVals = [];
     _.each(tableTriples, tt => {
-        if (tt.pred === "has_row") rows.push(_.last(_.split(tt.obj, "_")));
-        if (tt.pred === "has_value") subPreVals.push({"cell": _.last(_.split(tt.sub, "_")), "val": tt.obj});
-        else if (tt.pred === "has_column") columns.push(_.last(_.split(tt.obj, "_")));
+        if (tt.pred === "has_row"){
+            rows.push(_.last(_.split(tt.obj, "_")).match(/[a-zA-Z]+|[0-9]+/g));
+        } else if (tt.pred === "has_column"){
+            columns.push(_.last(_.split(tt.obj, "_")).match(/[a-zA-Z]+|[0-9]+/g));
+        }  else if (tt.pred === "has_value"){
+            subPreVals.push({"cell": _.last(_.split(tt.sub, "_")), "val": tt.obj});
+        } else {
+            objVals.push({"sub": tt.sub, "pred":tt.pred, "obj":tt.obj });
+        }
+    });
+    if (rows.length < 1 || columns.length < 1) return;
+    let rowNums = [];
+    let colNums = [];
+    _.each(_.flatten(rows), r => {
+        if (!isNaN(r)) rowNums.push(parseInt(r));
+    });
+    _.each(_.flatten(columns), c =>{
+       if(isNaN(c)) colNums.push(_.indexOf(alphabet, c.toLowerCase()));
     });
 
-    if (rows.length < 1 || columns.length < 1) return;
+    let fileds = [];
+    let data = [];
+    for(i = 0; i <= _.max(colNums); i++){
+        let fVal = _.filter(subPreVals, {cell: (alphabet[i].toUpperCase()+1)});
+        if (fVal.length != 1) fileds.push("");
+        else fileds.push(fVal[0].val);
+    }
 
-    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-    let theTable = [];
-
-    console.log(_.orderBy(rows));
-    console.log(_.orderBy(columns));
-    let maxRow = _.first(rows).match(/\d+/)[0];
-    let maxCol = _.indexOf(alphabet, _.first(columns).match(/[a-zA-Z]+|[0-9]+/g)[0])+1;
-    console.log(maxRow);
-    console.log(maxCol);
-    console.log(subPreVals);
-    /*
-    for(i=0; i<maxCol; i++){
-        theTable.push(i);
-        for(j=0; j<maxRow; j++){
-            theTable[i].push(j);
+    for(i=1; i<_.max(rowNums); i++){
+        let tempRow = [];
+        let dVal = _.filter(subPreVals, {cell: "A"+(i+1)});
+        if(dVal.length != 1){
+            tempRow.push("");
+        } else {
+            tempRow.push(dVal[0].val);
         }
+        if (tempRow[0]){
+            for(j=1; j<=_.max(colNums); j++){
+                let cv = _.filter(objVals, {sub:tempRow[0], pred: fileds[j]});
+                if(cv.length === 1){
+                    tempRow.push(cv[0].obj);
+                }else{
+                    tempRow.push("");
+                }
+            }
+        } else {
+            tempRow.push("");
+        }
+        data.push(tempRow);
     }
-    */
-
-    //console.log(theTable);
-
-    let thisFields = [];
-    for(let i = 0; i<maxCol+1; i++){
-
-    }
-
 
     let testInput = {
-        fields: ["Column 1", "Column 2"],
-        data: [
-            ["foo", "bar"],
-            ["abc", "def"]
-        ]};
+        fields: fileds,
+        data: data
+    };
 
     let csv = Papa.unparse(testInput);
     console.log(csv);
